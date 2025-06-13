3fc6885a52fc42977ec96cd73749eb0979f7cb0e
Oleksandr Panchuk <alekssen2008@gmail.com>
2025-06-13 22:41:43 +0300

    add hw8(fully)

--- 08_Pagination/main.js
+++ 08_Pagination/main.js
@@ -8,20 +8,71 @@ const pagesNav = document.querySelector('.pages nav ul');
 
 let limit = +numberInput.value;
 
+let currentPage = 1;
+
+let skipProducts = 0;
+
+let totalProducts = 0;
+
+function createPaginationButtons(totalProducts) {
+    pagesNav.innerHTML = '';
+    const totalPages = Math.ceil(totalProducts / limit);
+    const prevButton = `<li class="page-item"><a class="page-link" href="#">Previous</a></li>`;
+    pagesNav.innerHTML += prevButton;
+
+    let max = 0;
+    if (totalPages > 10) {
+        max = 10;
+    }
+    else{
+        max = totalPages;
+    }
+
+    for (let i = 1; i <= max; i++) {
+        const activeClass = i === currentPage ? 'active' : '';
+        pagesNav.innerHTML += `<li class="page-item ${activeClass}"><a class="page-link" href="#">${i}</a></li>`;
+    }
+
+    if (max != totalPages) {
+        const dots = `<li class="page-item disabled"><span class="page-link">...</span></li>`;
+        pagesNav.innerHTML += dots;
+        const activeClass = totalPages === currentPage ? 'active' : '';
+        pagesNav.innerHTML += `<li class="page-item ${activeClass}"><a class="page-link" href="#">${totalPages}</a></li>`;
+    }
+
+    const nextButton = `<li class="page-item"><a class="page-link" href="#">Next</a></li>`;
+    pagesNav.innerHTML += nextButton;
+}
+
+
+
 async function loadProducts()
 {
     try {
-        const response = await fetch('https://dummyjson.com/products');
+        resultDiv.innerHTML = '';
+        const response = await fetch(`https://dummyjson.com/products?limit=${limit}&skip=${skipProducts}`);
         const data = await response.json();
 
-        return data.products; // this
+        let products = data.products;
+        totalProducts = data.total;
+        createPaginationButtons(totalProducts);
+
+        for (const product of products) {
+            resultDiv.innerHTML += `<div class="card col my-1">
+                <img src="${product.thumbnail}" class="card-img-top" alt="...">
+                <div class="card-body">
+                <h5 class="card-title">${product.title}</h5>
+                <p class="card-text">${product.description}</p>
+                </div>
+            </div>`
+        }
     } catch (error) {
         console.error('Error fetching products:', error);
         resultDiv.innerHTML = '<h2 class="text-center col-12">Failed to load products</h2>';
     }
 }
 
-console.log(loadProducts());
+loadProducts();
 
 saveBtn.addEventListener('click', () => {
     limit = +numberInput.value;
@@ -29,7 +80,31 @@ saveBtn.addEventListener('click', () => {
         alert('Please enter a valid number');
         return;
     }
-    console.log(limit);
-    
-    
+    loadProducts();
+});
+
+pagesNav.addEventListener('click', (e) => {
+    let number = +e.target.innerText;
+    if (!isNaN(number)) {
+        currentPage = number;
+        skipProducts = (currentPage - 1) * limit;
+        setTimeout(loadProducts, 1000);
+    }
+    else {
+        if (e.target.innerText === 'Previous') {
+            if (currentPage > 1) {
+                currentPage--;
+                skipProducts = (currentPage - 1) * limit;
+                setTimeout(loadProducts, 1000);
+            }
+        }
+        else if (e.target.innerText === 'Next') {
+            if (currentPage < Math.ceil(totalProducts / limit)) 
+                {
+                    currentPage++;
+                    skipProducts = (currentPage - 1) * limit;
+                    setTimeout(loadProducts, 1000);
+                }
+        }
+    }
 });
